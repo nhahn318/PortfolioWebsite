2fc00efe8de38d18580308571fd0f4346b225727
TranThienNhan <nhtony318@gmail.com>
2025-10-22 00:58:28 +0700

    Fix: Move index.html and blog.html back to wwwroot and fix paths - resolve project loading issues

--- obj/Debug/net8.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca94121be3beb1e66c1aa8d123888c954b69ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531bd58c6fa2bdf24a6f60d60fce6b35ae6c019d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
